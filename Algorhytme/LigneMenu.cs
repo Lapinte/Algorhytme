c64de6701eed21c0b9d620d17ec6164969c8cff6
Adminl <Adminl@EMER07>
2018-05-31 14:48:02 +0200

    Ajout de la classe LigneMenu

---
+++ Algorhytme/LigneMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorhytme
+{
+    public class LigneMenu
+    {
+        public int Index { get; set; }
+        public string Texte { get; set; }
+    }
+}
