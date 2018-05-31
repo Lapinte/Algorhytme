bfb5ba43d9d7241b3b9e2a7d237048cefbd6aea6
Adminl <Adminl@EMER07>
2018-05-31 13:08:19 +0200

    Premier commit

---
+++ Algorhytme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorhytme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static int AfficherMenuEtRenvoyerSaisi(List<string> menu)
+        {
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+            }
+
+            Console.WriteLine("Votre choix :");
+
+            int saisi = int.Parse(Console.ReadLine());
+            return saisi;
+        }
+    }
+}
