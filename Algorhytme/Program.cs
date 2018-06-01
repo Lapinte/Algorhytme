15286f16f43d6b52d943b0808b40bd9716913bc0
Adminl <Adminl@EMER07>
2018-06-01 10:06:22 +0200

    ajout d'un menu

--- Algorhytme/Program.cs
+++ Algorhytme/Program.cs
@@ -10,20 +10,30 @@ namespace Algorhytme
     {
         static void Main(string[] args)
         {
+            //Création d'un menu composé de plusieurs lignes de type LigneMenu
+            List<LigneMenu> menu = new List<LigneMenu>
+            {
+                new LigneMenu { Numero = 1, Texte = "Aller au sous-menu"},
+                new LigneMenu { Numero = 3, Texte = "Aller au sous-sol"},
+                new LigneMenu { Numero = 42, Texte = "Aller en enfer"},
+            };
+
+            GererMenu(menu);
             
         }
         private static int GererMenu(List<LigneMenu> menu)
         {
+            //Affiche les lignes du menu
             foreach (LigneMenu ligne in menu)
             {
-                Console.WriteLine($"{ligne.Numero} {ligne.Texte}");
+                Console.WriteLine($"{ligne.Numero} {ligne.Texte.ToUpper()}");
             }
 
-
+            //Récupération de la saisie de l'utilisateur
             Console.WriteLine("Votre choix :");
-
             int saisi = int.Parse(Console.ReadLine());
 
+            //Test de la saisie par rapport aux lignes du menu
             while (true)
             {
                 foreach (LigneMenu ligne in menu)
